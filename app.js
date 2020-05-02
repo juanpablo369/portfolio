6cda1d4bdaba70bfdbdfd7d5041c1c2f07f8434e
juanpablo369 <52577050+juanpablo369@users.noreply.github.com>
2020-05-02 16:54:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,18 +26,6 @@ mongoose.connect(database.URI, {useUnifiedTopology: true,useNewUrlParser: true
 })
   .then(db => console.log('base de datos conectada'))
   .catch(err => console.log(err));
-//////////MIDLEWARES
-//Web-Push
-const publicVapidKey =
-  "BJ7mU7u4wZY1E87vlMKjX5D6isUWw0qFsEv1sGh-kpWMOrt9xS5cONGAqe4B3nXYh_EZ0KZ6RUdNPYbALDOpmEw";
-const privateVapidKey = "LHrk_X2oYozMSJDenUWcfcvqViCAG3Mn_JxW7i6B0mU";
-
-webpush.setVapidDetails(
-  "mailto:thor.devman@gmail.com",
-  publicVapidKey,
-  privateVapidKey
-);
-
 
 
 // view engine setup
@@ -71,32 +59,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
-
-
-
-// Subscribe Route
-app.post("/subscribe", (req, res) => {
-  // Get pushSubscription object
-  const subscription = req.body;
-
-  // Send 201 - resource created
-  res.status(201).json({});
-
-  // Create payload
-  const payload = JSON.stringify({
-		title: "Notificación de Pablo",
-		message: "Hello Nigga"+" son: "+cases+" casos confirmados"
-	});
-
-  // Pass object into sendNotification
-  webpush
-    .sendNotification(subscription, payload)
-    .catch(err => console.error(err));
-});
-
-//////////
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
